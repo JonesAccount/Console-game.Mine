08796c99c00fc3026036df66192ff8afe85981ad
nooneprofessor-art <nooneprofessor@gmail.com>
2026-01-30 19:32:37 +0500

    Add new file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from time import sleep
+from game import utils
 
 class InfoUser:
     __username = None
@@ -12,9 +13,10 @@ class InfoUser:
         for i in range(10):
             self.__NumbersCheckName.append(i)
         print("[🥳] Добро пожаловать в игру❕")
-        print("[📝] Прежде чем начать, узнаем о тебе\n")
+        print("[📝] Прежде чем начать, узнаем имя и ID\n")
         print(self.__line)
         self.timer()
+        self.GetUsername()
 
     def GetUsername(self):
         while True:
@@ -39,33 +41,78 @@ class InfoUser:
         print(f"[✅] Принято: {self.__username}")
         print(self.__line)
         self.timer()
-        self.ID()
+        self.GetID()
 
-    def ID(self):
-        while True:
-            try:
-                if self.__CounterID == True:
-                    self.__ID = int(input("[🤖] Твой ID из 4 цифр: "))
-                else:
-                    self.__ID = int(input("[☺️] Еще раз попробуй: "))
-                print(self.__line)
-                if len(str(self.__ID)) != 4:
-                    print("[🚫] Нужно ввести 4 цифры")
-                    self.__CounterID = False
-                    print(self.__line)
-                    continue
-                else:
-                    break
-            except ValueError:
-                self.__CounterID = False
-                print(self.__line)
-
-        print(f"[✅] Принято: ID:{self.__ID}")
+    def GetID(self):
+        self.__ID = id(self.__username)
+        print(f"[🔄] Формируем ID: 🆔{self.__ID}")
         print(self.__line)
-        self.timer()
+        self.start_game()
+
+
+    def start_game(self):
+        print("\n[🤖] Все готово, запускать игру?"); print("[1] - да😏      |      [0] - нет🫩")
+        print(self.__line)
+
+        while True:
+            action = input("[👤] ")
+            print(self.__line)
+            self.timer()
+            try:
+                action = int(action)
+                if type(action) == int:
+                    if action == 1 or action == 0:
+                        break
+                    else:
+                        print("[🚫] 1 или 2")
+                        print(self.__line)
+                        self.timer()
+            except ValueError:
+                print("[🚫] Ответь нормально")
+                print(self.__line)
+                self.timer()
+
+        if action == 1:
+            print("\n[🥰] Хорошей игры!", end="")
+            for i in range(5):
+                sleep(0.4)
+                print(".", end="")
+            utils.clear_console()
+            menu = Menu()
+            menu.start_menu()
+        else:
+            print("\n[🥲] Пока", end="")
+            for i in range(2):
+                sleep(0.8)
+            utils.clear_console()
+
 
     def timer(self):
         sleep(0.5)
 
+
+class Tutorial:
+    def __init__(self):
+        pass
+
+
+class Menu():
+    def start_menu(self):
+        print("""
+        ===== MINER SIMULATOR =====
+
+1. Играть / продолжить
+2. Профиль игрока
+3. Инвентарь
+4. Шахта
+5. Магазин
+6. Настройки
+7. Сохранить игру
+0. Выйти
+
+Выберите команду:
+        """)
+
+
 infouser = InfoUser()
-infouser.GetUsername()
+tutorial = Tutorial()
